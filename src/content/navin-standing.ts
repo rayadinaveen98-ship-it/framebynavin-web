bc64f764d86ed50f186e3a8b2b77590755ee775b
rayadinaveen98-ship-it <rayadinaveen98@gmail.com>
2026-09-19 20:45:27 +0530

    fix(v2): assemble verified standing portrait asset

---
+++ src/content/navin-standing.ts
@@ -0,0 +1,6 @@
+import { standingPart1 } from "./navin-standing-1";
+import { standingPart2 } from "./navin-standing-2";
+import { standingPart3 } from "./navin-standing-3";
+import { standingPart4 } from "./navin-standing-4";
+
+export const standingNavinDataUri = `data:image/webp;base64,${standingPart1}${standingPart2}${standingPart3}${standingPart4}`;
